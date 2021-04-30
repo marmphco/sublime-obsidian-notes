19982b98d1a3e2e380fb63c2302806177ed3a5f7
Matthew Jee <matthew@marmphco.com>
2021-04-30 00:14:00 -0700

    block embeds

--- obsidian.py
+++ obsidian.py
@@ -16,6 +16,30 @@ def collect_links(file):
             links.append(match[2:-2])
     return links
 
+def note_name_from_link(link: str) -> str:
+    # Link format is [[Name#Heading|Alias]]
+    # ignore the heading for now
+    location = link.split('|')[0]
+    return location.split('#')[0]
+
+def path_to_note(view: sublime.View, note_name: str) -> str:
+    # Search for a note with that name in the window's folders
+    # Return the first one we find.
+    for folder in view.window().folders():
+        for (root, dirs, files) in os.walk(folder):
+            for file in files:
+                if file.startswith(note_name):
+                    path = os.path.join(root, file)
+                    return path
+    return None
+
+def file_extension(path: str) -> str:
+    tokens = path.split('.')
+    if len(tokens) == 1:
+        return None
+    else:
+        return tokens[-1]
+
 # Opens [[Links]] to notes
 class ObsidianOpenNoteCommand(sublime_plugin.TextCommand):
 
@@ -23,7 +47,7 @@ class ObsidianOpenNoteCommand(sublime_plugin.TextCommand):
         # The command is only enabled within [[ ]]
         layout_pos = self.view.window_to_layout((event['x'], event['y']))
         text_pos = self.view.layout_to_text(layout_pos)
-        return self.view.match_selector(text_pos, 'meta.brackets markup.underline.link')
+        return self.view.match_selector(text_pos, 'meta.brackets markup.underline.link') or self.view.match_selector(text_pos, 'meta.brackets markup.raw.block')
 
     def want_event(self):
         return True
@@ -34,22 +58,15 @@ class ObsidianOpenNoteCommand(sublime_plugin.TextCommand):
         region = self.view.extract_scope(text_pos)
         link = self.view.substr(region)
 
-        # Link format is [[Name#Heading|Alias]]
-        # ignore the heading for now
-        location = link.split('|')[0]
-        note_name = location.split('#')[0]
+        note_name = note_name_from_link(link)
 
         log('open ' + note_name)
 
         # Search for a note with that name in the window's folders
         # open the first one we find.
-        for folder in self.view.window().folders():
-            for (root, dirs, files) in os.walk(folder):
-                for file in files:
-                    if file.startswith(note_name):
-                        path = os.path.join(root, file)
-                        self.view.window().open_file(path)
-                        return
+        path = path_to_note(self.view, note_name)
+        if path:
+            self.view.window().open_file(path)
 
 class Note:
 
@@ -123,6 +140,21 @@ class ObsidianListener(sublime_plugin.EventListener):
         # Update the index associated with the view's window
         self.index_for_view(view).update(view.window().folders())
 
+        # Display phantoms for embeds
+        self.phantom_set = sublime.PhantomSet(view)
+        phantoms = []
+        for region in view.find_by_selector('meta.brackets markup.raw.block'):
+            link = view.substr(region)
+            note_name = note_name_from_link(link)
+            note_path = path_to_note(view, note_name)
+            
+            if file_extension(note_name) == None:
+                note_content = open(note_path, 'r').read()
+                phantom = sublime.Phantom(region, note_content, sublime.LAYOUT_BLOCK)
+                phantoms.append(phantom)
+
+        self.phantom_set.update(phantoms)
+
     # File load handler
     def on_load(self, view):
         note = os.path.basename(view.file_name())[:-3]
@@ -141,7 +173,7 @@ class ObsidianListener(sublime_plugin.EventListener):
 
     # Completion handler
     def on_query_completions(self, view, prefix, locations):
-        
+
         # [[Link]] completion
         if view.match_selector(locations[0], 'meta.brackets'):
             index = self.index_for_view(view)
